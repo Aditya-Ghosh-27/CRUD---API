dd1feaa5f4afaa5b06ba02581bfa5fcb467ee9ad
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-20 18:17:15 +0530

    installed express-validator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express, { request, response } from "express"; // imports the entire express library from the node modules
+import { query, validationResult } from "express-validator";
+// The validationResult function in express-validator is used to extract the validation errors from a request and make them available in a Result object. This function takes a request object as its argument and returns a Result object. The Result object contains an array of ValidationError objects, which represent the validation errors that occurred during the request.
 const app = express(); // returns an instance of the exprress app
 
 const PORT = process.env.PORT || 3000; //So process.env.PORT || 3000 means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
@@ -14,7 +16,7 @@ const loggingMiddleware = (request, response, next) => {
 
 const resolveUserById = (request, response, next) => {
   const {
-    params: { id }
+    params: { id },
   } = request;
 
   const parsedId = parseInt(id);
@@ -27,7 +29,7 @@ const resolveUserById = (request, response, next) => {
   if (findUserIndex === -1) return response.sendStatus(404);
   request.findUserIndex = findUserIndex;
   next();
-}
+};
 
 // Registering a middleware function globally so that everytime I hit a specific route, the middleware gets called immidiately
 app.use(loggingMiddleware);
@@ -56,65 +58,78 @@ const mockUsers = [
 app.get("/", (request, response, next) => {
   console.log(`Base URl 1`);
   next();
-// },(request, response, next) => {
-//   console.log(`Base URl 2`);
-//   next();
-// },(request, response, next) => {
-//   console.log(`Base URL 3`);
-//   next();
-// }, (request, response) => {
+  // },(request, response, next) => {
+  //   console.log(`Base URl 2`);
+  //   next();
+  // },(request, response, next) => {
+  //   console.log(`Base URL 3`);
+  //   next();
+  // }, (request, response) => {
   return response.sendStatus(201).json({ msg: "Hello" });
 });
 
 // Query Parameters -> Query parameters are key-value pairs that are attached to the end of a URL to provide additional information to a web server. They appear to the right of the question mark in a URL, starting with ? behind the usual web address.
 
 // Learnt how to deal with query parameters
-app.get("/api/users", (request, response, next) => { //Another way of declaring a middleware function
-  console.log("Base URL");
-  // If we don't write the next() function then the request will hung up and not move onto the next middleware function
-  next();
-} ,function (request, response) {
-  console.log(request.query);
-  const {
-    query: { filter, value },
-  } = request; // since request is an object
-  // when filter and value are undefined
-  if (!filter && !value) {
+// I can use this validation chain to keep on calling more methods which in turn will return an instance of the validation.
+// query(filter) will make that it is a string and is also not empty
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage(" Must not be empty ")
+    .isLength({ min: 3, max: 10 })
+    .withMessage(" Must be atleast 3 - 10 characters "),
+  (request, response, next) => {
+    //Another way of declaring a middleware function
+    console.log("Base URL");
+    // If we don't write the next() function then the request will hung up and not move onto the next middleware function
+    next();
+  },
+  function (request, response) {
+    // console.log(request['express-validator#contexts']);
+    const result = validationResult(request);
+    console.log(result);
+    const {
+      query: { filter, value },
+    } = request; // since request is an object
+    // when filter and value are undefined
+    if (!filter && !value) {
+      return response.send(mockUsers);
+    }
+    if (filter && value) {
+      return response.send(
+        mockUsers.filter((user) => user.username.includes(value))
+      );
+    }
     return response.send(mockUsers);
   }
-  if (filter && value) {
-    return response.send(
-      mockUsers.filter((user) => user.username.includes(value))
-    );
-  }
-  return response.send(mockUsers);
-});
+);
 
-app.use(loggingMiddleware, function(request, response, next){
-  console.log('Finished logging...');
+app.use(loggingMiddleware, function (request, response, next) {
+  console.log("Finished logging...");
   next();
-})
+});
 
 // Route Parameters
 
 // request.params is an object which shows the route variable as an object and request.params.id will give us the value of the route variable.
 
-app.get(
-  "/api/users/:id", (request, response) => {
-    // Validation for your incoming GET requests
-    const parsedId = parseInt(request.params.id);
-    console.log(parsedId);
-    if (isNaN(parsedId)) {
-      return response.sendStatus(400).send({ msg: "Bad Request. Invalid ID " });
-    }
-    const findUser = mockUsers.find((user) => user.id === parsedId);
-    if (!findUser) {
-      return response.sendStatus(404);
-    }
-    return response.send(findUser);
-    // return response.send(mockUsers[parsedId - 1]);
+app.get("/api/users/:id", (request, response) => {
+  // Validation for your incoming GET requests
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return response.sendStatus(400).send({ msg: "Bad Request. Invalid ID " });
   }
-);
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) {
+    return response.sendStatus(404);
+  }
+  return response.send(findUser);
+  // return response.send(mockUsers[parsedId - 1]);
+});
 
 app.get("/api/products", (request, response) => {
   response.send([{ id: 1, product: "Chicken Shwarma", price: 90 }]);
@@ -159,11 +174,6 @@ app.delete("/api/users/:id", resolveUserById, (request, response) => {
 });
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
-
-
-
-
-
 
 // const {
 //   body,
